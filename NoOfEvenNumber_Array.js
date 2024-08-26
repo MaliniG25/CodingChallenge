285daacab10baf1153da37e78a304d04bc1e145c
malini250903 <MaliniG25>
2024-08-26 19:29:27 +0530

    Javascript Basics

--- NoOfEvenNumber_Array.js
+++
@@ -1,9 +0,0 @@
-let array=[10,5,6,8,22,44];
-let even_count=0;
-for (let i=0;i<=array.length;i++)
-{
-    if(array[i]%2==0){
-        even_count=even_count+1;
-    }
-}
-console.log("The no.of even element in the array is:",even_count);
